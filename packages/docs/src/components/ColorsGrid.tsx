6d53be73b36c873f0d2aa12298dfe702f51cb978
Gabriel Santos <gabriel.santos-2@syngenta.com>
2023-03-06 20:13:30 -0300

    Initial Commit

---
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -0,0 +1,16 @@
+import { colors } from '@radiance/tokens'
+import { getContrast } from 'polished'
+
+export function ColorsGrid() {
+  return Object.entries(colors).map(([key, color]) => {
+    const textColor = getContrast(color, '#FFF') < 3.5 ? '#000' : '#FFF'
+    return (
+      <div key={key} style={{ backgroundColor: color, padding: '2rem' }}>
+        <div style={{ display: 'flex', justifyContent: 'space-between', fontFamily: 'monospace', color: textColor }}>
+          <strong>${key}</strong>
+          <span>{color}</span>
+        </div>
+      </div>
+    )
+  })
+}
